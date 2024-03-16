4f7b5797f8072edfeb78192d4e79f6f896d25bd2
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-03-16 21:15:19 +0000

    fix: test address

--- app/page.tsx
+++ app/page.tsx
@@ -1,87 +1,79 @@
-import { getTokenUrl } from "frames.js";
+import { ClientProtocolId } from "frames.js";
 import {
   FrameButton,
   FrameContainer,
   FrameImage,
-  FrameReducer,
   NextServerPageProps,
+  getFrameMessage,
   getPreviousFrame,
-  useFramesReducer,
 } from "frames.js/next/server";
-import { base } from "viem/chains";
+import { getXmtpFrameMessage, isXmtpFrameActionPayload } from "frames.js/xmtp";
+import { DEBUG_HUB_OPTIONS } from "./debug/constants";
 
-type State = {
-  pageIndex: number;
-};
+const acceptedProtocols: ClientProtocolId[] = [
+  {
+    id: "xmtp",
+    version: "vNext",
+  },
+  {
+    id: "farcaster",
+    version: "vNext",
+  },
+];
 
-const nfts: {
-  src: string;
-  tokenUrl: string;
-}[] = [
-    {
-      src: "https://remote-image.decentralized-content.com/image?url=https%3A%2F%2Fipfs.decentralized-content.com%2Fipfs%2Fbafybeihod7ajntqhyibybkgnrlmgifhcjar45ra3ct4jimfmaokpokz5zu&w=1080&q=75",
-      tokenUrl: getTokenUrl({
-        address: "0x800243201fb33b86219315f5f44e1795dfb5d97a",
-        chain: base,
-        tokenId: "28"
-      }),
-    },
-    {
-      src: "https://remote-image.decentralized-content.com/image?url=https%3A%2F%2Fipfs.decentralized-content.com%2Fipfs%2Fbafybeigs6jkboqjn4admqyr2nz7googp6r6xezedx54yaowrjtq5sad22y&w=1080&q=75",
-      tokenUrl: getTokenUrl({
-        address: "0xde94e589406eb064ca0cdbe22939201312aa0656",
-        chain: base,
-        tokenId: "premint-2"
-      }),
-    },
-    {
-      src: "https://remote-image.decentralized-content.com/image?url=https%3A%2F%2Fipfs.decentralized-content.com%2Fipfs%2Fbafybeibpgna6dmlpvmhmg6eueyyolzcxlkutltbm7g77a36scrsw4v4khy&w=1080&q=75",
-      tokenUrl: getTokenUrl({
-        address: "0xde94e589406eb064ca0cdbe22939201312aa0656",
-        chain: base,
-        tokenId: "premint-1"
-      }),
-    },
-  ];
-const initialState: State = { pageIndex: 0 };
-
-const reducer: FrameReducer<State> = (state, action) => {
-  const buttonIndex = action.postBody?.untrustedData.buttonIndex;
-
-  return {
-    pageIndex: buttonIndex
-      ? (state.pageIndex + (buttonIndex === 2 ? 1 : -1)) % nfts.length
-      : state.pageIndex,
-  };
-};
-
+// This is a react server component only
 export default async function Home({
   params,
   searchParams,
 }: NextServerPageProps) {
-  const previousFrame = getPreviousFrame<State>(searchParams);
-  const [state] = useFramesReducer<State>(reducer, initialState, previousFrame);
+  const previousFrame = getPreviousFrame(searchParams);
+
+  let fid: number | undefined;
+  let walletAddress: string | undefined;
+
+  if (
+    previousFrame.postBody &&
+    isXmtpFrameActionPayload(previousFrame.postBody)
+  ) {
+    const frameMessage = await getXmtpFrameMessage(previousFrame.postBody);
+    walletAddress = frameMessage?.verifiedWalletAddress;
+  } else {
+    const frameMessage = await getFrameMessage(
+      previousFrame.postBody,
+      DEBUG_HUB_OPTIONS
+    );
+
+    if (frameMessage && frameMessage?.isValid) {
+      fid = frameMessage?.requesterFid;
+      walletAddress =
+        frameMessage?.requesterCustodyAddress.length > 0
+          ? frameMessage?.requesterCustodyAddress
+          : frameMessage.requesterCustodyAddress;
+    }
+  }
 
   return (
     <div>
-      <h1>Leovido gallery</h1>
-      <p>What are you doing here anyways? Get back to Warpcast!</p>
-
       <FrameContainer
         postUrl="/frames"
         pathname="/"
-        state={state}
+        state={{}}
         previousFrame={previousFrame}
+        accepts={acceptedProtocols}
       >
-        <FrameImage
-          src={nfts[state.pageIndex]!.src}
-          aspectRatio="1:1"
-        ></FrameImage>
-        <FrameButton>←</FrameButton>
-        <FrameButton>→</FrameButton>
-        <FrameButton action="mint" target={nfts[state.pageIndex]!.tokenUrl}>
-          Mint
-        </FrameButton>
+        <FrameImage>
+          <div tw="flex flex-col">
+            <div tw="flex">
+              This frame gets the interactor&apos;s wallet address or FID
+              depending on the client protocol.
+            </div>
+            {fid && <div tw="flex">FID: {fid}</div>}
+            {walletAddress && (
+              <div tw="flex">Wallet Address: {walletAddress}</div>
+            )}
+          </div>
+        </FrameImage>
+        <FrameButton>Check</FrameButton>
       </FrameContainer>
     </div>
   );
