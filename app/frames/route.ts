b18ab33e4a4397a5092f149174e1c25c49d02cd6
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-03-18 14:21:28 +0000

    fix: testing

--- app/frames/route.ts
+++ app/frames/route.ts
@@ -8,20 +8,49 @@ const MAXIMUM_KV_RESULT_LIFETIME_IN_SECONDS = 2 * 60; // 2 minutes
 
 export async function POST(req: NextRequest): Promise<Response> {
   const body = await req.json();
+  // Parse and validate the frame message
+  const { isValid, message } = await validateFrameMessage(body.trustedData.messageBytes);
 
-  const { isValid, message } = await validateFrameMessage(body);
   if (!isValid || !message) {
-    return new NextResponse("Invalid message", { status: 400 });
+    return new NextResponse(`
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Error</title>
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="${process.env.GATEWAY_URL}/ipfs/QmdHqdPuFrD5LTCtBrf6pio41m53cj54De3LS7qsxyVpNV/ur_in.jpg" />
+                <meta property="fc:frame:button:1" content="Visit dividoge.com" />
+                <meta property="fc:frame:button:1:action" content="post_redirect" />
+                <meta property="fc:frame:button:2" content="Join the $MILO channel" />
+                <meta property="fc:frame:button:2:action" content="post_redirect" />
+                <meta property="fc:frame:post_url" content="${process.env.BASE_URL}/api/end" />
+            </head>
+        </html>
+                `);
   }
 
-  const frameMessage = await getFrameMessage(body.postBody, {
-    ...DEBUG_HUB_OPTIONS,
-  });
-  console.warn(frameMessage, 'hereeee')
+  // verify independently
+  console.warn('valid')
+  const frameMessage = await getFrameMessage(body.postBody);
+
+  return new NextResponse(`
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Success</title>
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="${process.env.GATEWAY_URL}/ipfs/QmdHqdPuFrD5LTCtBrf6pio41m53cj54De3LS7qsxyVpNV/ur_in.jpg" />
+                <meta property="fc:frame:button:1" content="Visit dividoge.com" />
+                <meta property="fc:frame:button:1:action" content="post_redirect" />
+                <meta property="fc:frame:button:2" content="Join the $MILO channel" />
+                <meta property="fc:frame:button:2:action" content="post_redirect" />
+                <meta property="fc:frame:post_url" content="${process.env.BASE_URL}/api/end" />
+            </head>
+        </html>
+                `);
   const uniqueId = `fid:${frameMessage.requesterFid}`;
 
   try {
-    // TODO use frames.js to fetch wallet
     const walletAddress = frameMessage.requesterCustodyAddress
     await kv.set<AddressModel>(
       uniqueId,
