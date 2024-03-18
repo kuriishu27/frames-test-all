e88b5c1c302118efe48aa468d753a70799f19056
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-03-18 12:19:19 +0000

    fix: redis

--- app/frames/route.ts
+++ app/frames/route.ts
@@ -9,22 +9,19 @@ const MAXIMUM_KV_RESULT_LIFETIME_IN_SECONDS = 2 * 60; // 2 minutes
 export async function POST(req: NextRequest): Promise<Response> {
   const body = await req.json();
 
-  // Parse and validate the frame message
-  // const { isValid, message } = await validateFrameMessage(body);
-  // if (!isValid || !message) {
-  //   console.warn('invalid hereeeee')
-  //   return new NextResponse("Invalid message", { status: 400 });
-  // }
+  const { isValid, message } = await validateFrameMessage(body);
+  if (!isValid || !message) {
+    return new NextResponse("Invalid message", { status: 400 });
+  }
 
-  console.warn(body, 'body')
-  // verify independently
   const frameMessage = await getFrameMessage(body.postBody, {
     ...DEBUG_HUB_OPTIONS,
   });
+  console.warn(frameMessage, 'hereeee')
   const uniqueId = `fid:${frameMessage.requesterFid}`;
 
   try {
-
+    // TODO use frames.js to fetch wallet
     const walletAddress = frameMessage.requesterCustodyAddress
     await kv.set<AddressModel>(
       uniqueId,
@@ -36,21 +33,11 @@ export async function POST(req: NextRequest): Promise<Response> {
       { ex: MAXIMUM_KV_RESULT_LIFETIME_IN_SECONDS }
     );
 
-    return new NextResponse(`
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <title>Congrats you are on the $MILO whitelist.</title>
-                <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="${process.env.GATEWAY_URL}/ipfs/QmdHqdPuFrD5LTCtBrf6pio41m53cj54De3LS7qsxyVpNV/ur_in.jpg" />
-                <meta property="fc:frame:button:1" content="Visit dividoge.com" />
-                <meta property="fc:frame:button:1:action" content="post_redirect" />
-                <meta property="fc:frame:button:2" content="Join the $MILO channel" />
-                <meta property="fc:frame:button:2:action" content="post_redirect" />
-                <meta property="fc:frame:post_url" content="${process.env.BASE_URL}/api/end" />
-            </head>
-        </html>
-                `);
+    return NextResponse.json({
+      data: walletAddress,
+      status: "success",
+      timestamp: new Date().getTime(),
+    });
   } catch (e) {
     await kv.set<AddressModel>(
       uniqueId,
